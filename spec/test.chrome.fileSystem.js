1aae4b113baf5788168c1dd0304044df517f4541
Max Woghiren <max.woghiren@gmail.com>
2013-01-25 14:20:18 -0500

    Added chrome.fileSystem writable entry methods.
    These are shell implementations; isWritableEntry returns false and getWritableEntry returns null.
    Also added simple tests.

--- spec/test.chrome.fileSystem.js
+++ spec/test.chrome.fileSystem.js
@@ -5,12 +5,12 @@
 
 chromeSpec(function(runningInBackground) {
   describe('chrome.fileSystem', function() {
+    // Create a file entry for testing.
+    var FileEntry = cordova.require('cordova/plugin/FileEntry');
+    var fileEntry = new FileEntry('filename', 'fullpath');
+
     describe('getDisplayPath()', function() {
       it('returns the full path of the given file entry', function() {
-        // Create a file entry.
-        var FileEntry = cordova.require('cordova/plugin/FileEntry');
-        var fileEntry = new FileEntry('filename', 'fullpath');
-
         // Create the callback.
         var getDisplayPathCallback = function(displayPath) {
           expect(displayPath).toEqual(fileEntry.fullPath);
@@ -18,6 +18,28 @@ chromeSpec(function(runningInBackground) {
 
         // Get the display path.
         chrome.fileSystem.getDisplayPath(fileEntry, getDisplayPathCallback);
+      });
+    });
+    describe('getWritableEntry()', function() {
+      it('returns null', function() {
+        // Create the callback.
+        var getWritableEntryCallback = function(writableFileEntry) {
+          expect(writableFileEntry).toBeNull();
+        };
+
+        // Get the writable file entry.
+        chrome.fileSystem.getWritableEntry(fileEntry, getWritableEntryCallback);
+      });
+    });
+    describe('isWritableEntry()', function() {
+      it('returns false', function() {
+        // Create the callback.
+        var isWritableEntryCallback = function(isWritable) {
+          expect(isWritable).toBeFalsy();
+        };
+
+        // Get whether the file entry is writable.
+        chrome.fileSystem.isWritableEntry(fileEntry, isWritableEntryCallback);
       });
     });
   });
